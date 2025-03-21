c12d12b2e8654db4befda218089547bd4e8d4a0d
Bringiton24 <154008897+Bringiton24@users.noreply.github.com>
2025-03-21 10:43:27 +0800

    21/03/2025
    Fixed the Finance Controller naming from /finance to /finance-manager and updated it to fetch data from the API.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\VideoController;
 use App\Http\Controllers\VideoViolationsController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\FinanceController;
 
 
 Route::get('/', function () {
@@ -26,11 +27,7 @@ Route::middleware(['auth', 'role:content-admin|super-admin'])->group(function ()
 
 Route::middleware(['auth', 'role:super-admin'])->group(function () {
     Route::get('/super-admin', [SuperAdminController::class, 'superAdmin'])->name('super.admin');
-    // Route::put('/videos/{video}/status', action: [SuperAdminController::class, 'updateStatus']);
-// });
-
-// Route::middleware(['auth', 'role:finance-admin|super-admin'])->group(function () {
-    Route::get('/finance-manager', [FinanceController::class, 'payoutdetails']);// });
+});
 
 Route::get('/videos', function () {
     return Inertia::render('VideoManagement', [
@@ -43,18 +40,4 @@ Route::get('/videos', function () {
 Route::put('/videos/{id}/violations', [VideoViolationsController::class, 'update']);
 Route::put('videos/{video}/violations', [VideoViolationsController::class, 'update']);
 
-Route::get('/finance', function () {
-    $bookings = \App\Models\Booking::all();
-    $users = \App\Models\User::all();
-    $listings = \App\Models\Listing::all();
-    $payout_methods = \App\Models\PayoutMethod::all();
-    $invoices = \App\Models\Invoice::all();
-
-    return Inertia::render('FinanceManager', [
-        'bookings' => $bookings,
-        'users' => $users,
-        'listings' => $listings,
-        'payoutMethods' => $payout_methods,
-    ]);
-});
-
+Route::get('/finance-manager', [FinanceController::class, 'payoutdetails']);
