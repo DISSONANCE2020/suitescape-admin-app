c254f40400fcddb96c17a6621f595f1930a80a61
DISSONANCE2020 <132447240+DISSONANCE2020@users.noreply.github.com>
2025-04-28 16:08:04 +0800

    modified:   resources/js/Components/FinanceRefundsTable.jsx

--- resources/js/Components/FinanceRefundsTable.jsx
+++ resources/js/Components/FinanceRefundsTable.jsx
@@ -155,11 +155,11 @@ const FinanceRefundsTable = ({
                                                     className={`px-3 py-1 text-white w-[160px] font-bold block mx-auto rounded-md ${
                                                         refundStatus ===
                                                         "REFUND PENDING"
-                                                            ? "bg-[#EF4444]" // Red for Refund Pending
+                                                            ? "bg-[#EF4444]" 
                                                             : refundStatus ===
                                                               "REFUND ISSUED"
-                                                            ? "bg-[#10B981]" // Green for Refund Issued
-                                                            : "bg-[#D1D5DB]" // Default gray for N/A
+                                                            ? "bg-[#10B981]" 
+                                                            : "bg-[#D1D5DB]" 
                                                     }`}
                                                 >
                                                     {refundStatus}
