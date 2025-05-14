e55df78a6a6affc0c4155e190e40adb67bc67096
Bringiton24 <tcbtolentino@gmail.com>
2025-05-14 16:26:35 +0800

    14/05/2025
    remove error @guest
    
    @endguest

--- resources/js/Components/FinancePayoutDetails.jsx
+++ resources/js/Components/FinancePayoutDetails.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import FinanceListingDetailsModal from "./FinanceListingDetailsModal";
-@guest
-
-@endguest
 const FinancePayoutDetails = ({
     booking,
     users,
